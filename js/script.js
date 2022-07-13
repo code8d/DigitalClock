1ccf4a9f9ceb69cc8201e37eceddac3240009591
code8d <code8d@gmail.com>
2022-07-13 23:45:04 +0500

    Adapted for mobile, small css and script.js fix

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ const day = document.querySelector('.day')
 const time = document.querySelector('.time')
 const year = document.querySelector('.year')
 
-console.log(day);
 setInterval(() => day.textContent = dayName(), 100)
 
 function dayName() {
@@ -12,8 +11,6 @@ function dayName() {
 
     return days[new Date().getDay()]
 }
-
-dayName()
 
 setInterval(() => time.textContent = `${new Date().getHours()} : ${new Date().getMinutes()} : ${new Date().getSeconds()}`, 100)
 
